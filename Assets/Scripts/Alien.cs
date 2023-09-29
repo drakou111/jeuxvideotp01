33582bab0d86dc01551c348e0d5a6e0c4ba5d96c
Samuel Potvin <2133123@csfoy.ca>
2023-09-29 11:30:24 -0400

    merge

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Alien : MonoBehaviour
 {
+    [SerializeField] int health = 2;
+    [SerializeField] int damage = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            this.health = 0;
+            collision.gameObject.GetComponent<Player>().hit(damage);
+        }
+
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (this.health <= 0 && gameObject.active)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void hit(int health)
+    {
+        this.health -= health;
+        if (this.health < 0)
+        {
+            this.health = 0;
+        }
     }
 }
