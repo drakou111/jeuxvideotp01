502a819c300eb3b17c9aeb6365a04777b4e4f8b3
Samuel Potvin <2133123@csfoy.ca>
2023-09-29 12:44:25 -0400

    Ajout du pool pour les aliens et les spawners

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -4,55 +4,48 @@ using UnityEngine;
 
 public class Alien : MonoBehaviour
 {
-  [SerializeField] int health = 2;
-  [SerializeField] int damage = 1;
+    [SerializeField] int health = 2;
+    [SerializeField] int damage = 1;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-
-  }
-
-  private void OnCollisionEnter(Collision collision)
-  {
-    if (collision.gameObject.CompareTag("Player"))
-    {
-      this.health = 0;
-      collision.gameObject.GetComponent<Player>().hit(damage);
-    }
-
-    if (collision.gameObject.CompareTag("Bullet"))
+    // Start is called before the first frame update
+    void Start()
     {
 
     }
-  }
 
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (this.health <= 0 && gameObject.active)
+    private void OnCollisionEnter(Collision collision)
     {
-      if (collision.gameObject.CompareTag("Player"))
-      {
-        this.health = 0;
-        collision.gameObject.GetComponent<Player>().hit(damage);
-      }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            this.health = 0;
+            collision.gameObject.GetComponent<Player>().hit(damage);
+        }
 
-      if (collision.gameObject.CompareTag("Bullet"))
-      {
-        int damage = collision.gameObject.GetComponent<Bullet>().getDamage();
-        hit(damage);
-      }
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            int damage = collision.gameObject.GetComponent<Bullet>().getDamage();
+            hit(damage);
+            //TODO: disable bullet
+        }
     }
-  }
 
-  void hit(int health)
-  {
-    this.health -= health;
-    if (this.health < 0)
+
+    // Update is called once per frame
+    void Update()
     {
-      this.health = 0;
+        if (this.health <= 0 && gameObject.activeSelf)
+        {
+            this.health = 0;
+            gameObject.SetActive(false);
+        }
     }
-  }
+
+    void hit(int health)
+    {
+        this.health -= health;
+        if (this.health < 0)
+        {
+            this.health = 0;
+        }
+    }
 }
--- Assets/Scripts/SpawnerManager.cs
+++ Assets/Scripts/SpawnerManager.cs
@@ -1,41 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using UnityEngine;
 
 public class SpawnerManager : MonoBehaviour
 {
-  [SerializeField] Spawner spawner;
-  private static int spawnerNumber = 8;
+    [SerializeField] Spawner spawner;
+    [SerializeField] Alien alien;
+    private static int spawnerNumber = 8;
+    private static int alienNumber = 20;
 
-  private Spawner[] spawners = new Spawner[spawnerNumber];
+    private Spawner[] spawners = new Spawner[spawnerNumber];
+    private Alien[] aliens = new Alien[alienNumber];
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    for (int i = 0; i < spawnerNumber; i++)
+    // Start is called before the first frame update
+    void Start()
     {
-      spawners[i] = Instantiate(spawner);
-      spawners[i].gameObject.SetActive(false);
+        for (int i = 0; i < spawnerNumber; i++)
+        {
+            spawners[i] = Instantiate(spawner);
+            spawners[i].gameObject.SetActive(false);
+        }
+
+        for (int i = 0; i < alienNumber; i++)
+        {
+            aliens[i] = Instantiate(alien);
+            aliens[i].gameObject.SetActive(false);
+        }
+
     }
-  }
 
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
-
-  private void spawnSpawner()
-  {
-    for (int i = 0; i < spawnerNumber; i++)
+    // Update is called once per frame
+    void Update()
     {
-      if (!spawners[i].gameObject.activeSelf)
-      {
-        spawners[i].gameObject.SetActive(true);
-        return;
-      }
+
     }
-  }
+
+    private void spawnSpawner()
+    {
+        for (int i = 0; i < spawnerNumber; i++)
+        {
+            if (!spawners[i].gameObject.activeSelf)
+            {
+                spawners[i].gameObject.SetActive(true);
+                return;
+            }
+        }
+    }
+
+    private void spawnAlien() {
+        for (int i = 0; i < alienNumber; i++)
+        {
+            if (!aliens[i].gameObject.activeSelf)
+            {
+                aliens[i].gameObject.SetActive(true);
+                Spawner randomSpawner = getRandomSpawner();
+
+                aliens[i].transform.position = randomSpawner.transform.position;
+                return;
+            }
+        }
+    }
+
+    private Spawner getRandomSpawner()
+    {
+        int numberValidSpawners = getEnabledSpawnersCount();
+        Spawner[] validSpawners = new Spawner[numberValidSpawners];
+        if (validSpawners.Length <= 0) {
+            return null;
+        }
+        int offset = 0;
+        for (int i = 0; i < spawnerNumber; i++)
+        {
+            if (spawners[i].gameObject.activeSelf)
+            {
+                validSpawners[offset] = spawners[i];
+                offset++;
+            }
+        }
+
+
+        int random = Random.Range(0, numberValidSpawners);
+        return validSpawners[random];
+    }
+
+    private int getEnabledSpawnersCount() {
+        int count = 0;
+        for (int i = 0; i < spawnerNumber; i++) {
+            if (spawners[i].gameObject.activeSelf) {
+                count++;
+            }
+        }
+        return count;
+    }
 
 
 }
